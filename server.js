afdbd31c28fa2df12c1c1ca9805bf17da6b74245
GlauberViniciusCB <g_viniciuscb@outlook.com>
2026-01-03 21:05:24 -0300

    feat: criando servidor

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("Servidor Da Barbearia Funcionando!");
+});
+
+app.get('/api/servicos',(req, res) => {
+    const servicos = [
+        {nome: 'Corte', quantidade: 18982},
+        {nome: 'Barbaterapia', quantidade: 3990}
+    ];
+    res.json(servicos);
+});
+
+const PORT = 3000;
+app.listen(PORT,() =>{
+    console.log(`Servidor rodando em: http://localhost:${PORT}`)
+})
